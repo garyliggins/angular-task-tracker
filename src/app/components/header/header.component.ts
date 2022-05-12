cb734f7117043bd909feb0caff55ff3eff95c250
Gary Liggins <GLiggins@kmbsus.local>
2022-05-11 18:08:58 -0700

    finished

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -7,14 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'task tracker';
+  showAddTask: boolean | undefined;
+  subscription!: Subscription;
 
-  constructor() { }
+
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService. onToggle().subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    alert("toggle foo");
+    this.uiService.toggleAddTask();
   }
 
 }
